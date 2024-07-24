35fb021dc320805089b695910a9583852c8ae154
david <ddnn2026@gmail.com>
2024-07-24 17:36:13 +0700

    env file

--- config/db.go
+++ config/db.go
@@ -1,15 +1,37 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/ddcad2030/gin-gorm-rest/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func ConnectDB() {
-	dsn := "host=db_postgres user=root password=root dbname=db_test port=5432"
+	LoadEnv()
+	dsn := fmt.Sprintf(
+		"host=localhost user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST_PORT"),
+	)
+
+	// log.Println(dsn)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
